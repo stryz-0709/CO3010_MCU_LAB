3588d27cce567f9359354e128561d23cb8930636
stryz_0709 <minhtriinh123@gmail.com>
2023-09-25 07:48:02 +0700

    Update main.c

--- SRC/Core/Src/main.c
+++ SRC/Core/Src/main.c
@@ -89,11 +89,11 @@ uint16_t pins[12] = {LED_0_Pin, LED_1_Pin, LED_2_Pin, LED_3_Pin,
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
   void clearAllClock(){
-	  for (int i = 0; i < 12; i++) HA_GPIO_WritePin(GPIOA, pins[i], SET);
+	  for (int i = 0; i < 12; i++) HAL_GPIO_WritePin(GPIOA, pins[i], RESET);
   }
 
   void setNumberOnClock(int num){
-	  HAL_GPIO_WritePin(GPIOA, pins[num], RESET);
+	  HAL_GPIO_WritePin(GPIOA, pins[num], SET);
   }
   /* USER CODE END 2 */
 
